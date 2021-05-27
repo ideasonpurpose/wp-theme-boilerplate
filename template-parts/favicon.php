bb6c8754006c6a25de44254fc881565f13c0c137
Joe Maller <joe@joemaller.com>
2021-05-27 18:03:58 -0400

    placeholder favicon to stop extra request errors

--- template-parts/favicon.php
+++ template-parts/favicon.php
@@ -1,5 +1,12 @@
 <!-- START template-parts/favicon.php -->
 
-<!-- use https://realfavicongenerator.net/  -->
+<?php
+/**
+ * Replace the contents of this file with tags from https://realfavicongenerator.net/
+ * Favicon assets should live in `src/favicon/`
+ * Reference favicon files from this path: `<?= get_template_directory_uri(); ?>/dist/favicon/`
+ */
+?>
+<link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/dist/favicon/favicon.ico">
 
 <!-- END template-parts/favicon.php -->
